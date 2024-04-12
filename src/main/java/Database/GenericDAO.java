13d9b63532b397836b925bc068f1dadb67e4d8a6
unknown <solokokone8@gmail.com>
2024-04-12 12:20:42 +0700

    create model

--- src/main/java/Database/GenericDAO.java
+++ src/main/java/Database/GenericDAO.java
@@ -5,13 +5,15 @@ import java.util.List;
 import RowMaper.RowMapper;
 
 public interface GenericDAO<T> {
-	<T> List<T> querry(String sql, RowMapper<T> rowMapper, Object... objects);
+    <T> List<T> querry(String sql, RowMapper<T> rowMapper, Object... objects);
 
-	int save(String sql, Object... objects);
+    <T> List<T> querry(String sql, RowMapper<T> rowMapper, Object... objects,);
 
-	boolean update(String sql, Object... objects);
-	
-	int count(String sql, Object... objects);
-	
-	int query(String sql, Object... objects);
+    int save(String sql, Object... objects);
+
+    boolean update(String sql, Object... objects);
+
+    int count(String sql, Object... objects);
+
+    int query(String sql, Object... objects);
 }
