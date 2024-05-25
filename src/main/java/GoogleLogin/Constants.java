3f8650b894af32f793565ed7412fc513e857fbb7
dubanteo19 <21130444@st.hcmuaf.edu.vn>
2024-05-25 12:06:38 +0700

    create Google login

---
+++ src/main/java/GoogleLogin/Constants.java
@@ -0,0 +1,10 @@
+package GoogleLogin;
+
+public class Constants {
+    public static final String GOOGLE_CLIENT_ID = "590343439409-4lslffgpgrj4apstvj4plsu3864bf6iv.apps.googleusercontent.com";
+    public static final String GOOGLE_CLIENT_SECRET = "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";
+    public static final String GOOGLE_REDIRECT_URI = "http://localhost:8080/GoogleLogin";
+    public static final String GOOGLE_GRANT_TYPE = "authorization_code";
+    public static final String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+    public static final String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+}
