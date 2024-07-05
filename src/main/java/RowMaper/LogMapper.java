270aee85daf2cb3b692d7c9fb68b044ad4598f52
dubanteo19 <21130444@st.hcmuaf.edu.vn>
2024-07-06 02:48:45 +0700

    add find all log method

---
+++ src/main/java/RowMaper/LogMapper.java
@@ -0,0 +1,24 @@
+package RowMaper;
+
+import Model.Log;
+import Model.LogLevel;
+import RowMaper.column.LogsColumn;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LogMapper implements RowMapper<Log> {
+    @Override
+    public Log map(ResultSet r) throws SQLException {
+        Log log = new Log();
+        log.setId(r.getInt(LogsColumn.LogId.name()));
+        log.setDescription(r.getString(LogsColumn.Description.name()));
+        log.setNation(r.getString(LogsColumn.Level.name()));
+        log.setLevel((LogLevel) r.getObject(LogsColumn.Level.name()));
+        log.setIpAddress(r.getString(LogsColumn.IpAddress.name()));
+        log.setCurrentValue(r.getString(LogsColumn.CurrentValue.name()));
+        log.setAfterValue(r.getString(LogsColumn.AfterValue.name()));
+        log.setUrl(r.getString(LogsColumn.Url.name()));
+        return log;
+    }
+}
---
+++ src/main/java/RowMaper/column/LogsColumn.java
@@ -0,0 +1,8 @@
+package RowMaper.column;
+
+public enum LogsColumn {
+    LogId, IpAddress,
+    Url, Nation,
+    CurrentValue, AfterValue,
+    Description, Level
+}
