b9711cda264a23c28f9d25d380a8146ed0611bd2
dubanteo19 <21130444@st.hcmuaf.edu.vn>
2024-07-08 00:29:03 +0700

    feat: create getLogLevel method from level create get nation from ipaddress

--- src/main/java/Services/MLogFactory.java
+++ src/main/java/Services/MLogFactory.java
@@ -1,13 +1,41 @@
 package Services;
 
 import Model.Log;
+import Model.LogLevel;
 
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MLogFactory {
-    public static Log getLog(HttpServletRequest httpServletRequest) {
+    public static Map<Integer, LogLevel> logMap = new HashMap<Integer, LogLevel>();
+
+    static {
+        logMap.put(1, LogLevel.INFO);
+        logMap.put(2, LogLevel.ALERT);
+        logMap.put(3, LogLevel.WARNING);
+        logMap.put(4, LogLevel.DANGER);
+    }
+
+    public static LogLevel getLogLevel(int level) {
+        return logMap.get(level);
+    }
+
+    public static Log getLog(HttpServletRequest hquest, HttpServlet httpServlet, int logLevel) {
         Log log = new Log();
-        log.setIpAddress(httpServletRequest.getRemoteAddr());
+        String ipAddress = hquest.getRemoteAddr();
+        log.setIpAddress(ipAddress);
+        log.setUrl(httpServlet.getServletName());
+        log.setDescription("View");
+        log.setAfterValue("");
+        log.setCurrentValue("");
+        log.setNation(getNation(ipAddress));
+        log.setLevel(getLogLevel(logLevel));
         return log;
+    }
+
+    private static String getNation(String ipAddress) {
+        return "Viet Nam";
     }
 }
